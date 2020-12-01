6fc00eaa172d54d3e9038116cf0acee5d88e7a8a
Claydefroosch <patrick.kuehlen@yahoo.de>
2020-12-01 17:43:04 +0100

    Trying

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # its-a-game
+
 My first own project to build a multiplayer game with socketIO.
+basicUrl + movieId + "?api_key=" +
---
+++ public/js/script.js
@@ -0,0 +1,37 @@
+new Vue({
+  el: "#main",
+  data: {
+    message: "Welcome to the Quiz",
+    greetee: "World",
+    basicUrl: "https://api.themoviedb.org/3/movie/",
+    movieId: parseInt(Math.random() * (25000 - 1 + 1), 10) + 1,
+    title: null,
+    description: null,
+    Apikey: null,
+  },
+  created: function () {
+    console.log("created");
+  },
+  mounted: function () {
+    console.log("mounted");
+    axios.get("/getMovie").then((result) => {
+      console.log("this is the result", result);
+    });
+  },
+
+  updated: function () {
+    console.log("updated");
+  },
+  methods: {
+    clicker: function () {
+      console.log("clicked!!");
+      this.greetee = "Kitty";
+    },
+    addCologne: function () {
+      this.cities.push({
+        name: "Cologne",
+        country: "Germany",
+      });
+    },
+  },
+});
